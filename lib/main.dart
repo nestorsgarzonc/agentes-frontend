cd953abc212bc592b403029ec63af5724256ff81
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-28 10:32:57 -0500

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:diner/app.dart';
+import 'package:oyt_front_core/push_notifications/push_notif_provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   EquatableConfig.stringify = true;
   initializeDateFormatting('es_CO', null);
   await Hive.initFlutter();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  PushNotificationProvider.setupInteractedMessage();
   runApp(const ProviderScope(child: MyApp()));
 }
