da534f42ae8df495f4493221523d951936dfdd65
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-29 12:15:40 -0500

    Fix notifications on foreground

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:equatable/equatable.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:diner/app.dart';
-import 'package:oyt_front_core/push_notifications/push_notif_provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -13,9 +13,12 @@ void main() async {
   EquatableConfig.stringify = true;
   initializeDateFormatting('es_CO', null);
   await Hive.initFlutter();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  PushNotificationProvider.setupInteractedMessage();
+  if (kIsWeb) {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  } else {
+    await Firebase.initializeApp(
+      options: Firebase.apps.isNotEmpty ? DefaultFirebaseOptions.currentPlatform : null,
+    );
+  }
   runApp(const ProviderScope(child: MyApp()));
 }
