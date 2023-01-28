5525f907273d1b4a9ee7d8e3c75f9b6bda43f371
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-28 11:18:09 -0500

    Fix deploy

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import firebase_core
 import firebase_messaging
 import mobile_scanner
-import path_provider_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
